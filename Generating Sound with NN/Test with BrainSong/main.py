b2f1a0ead466ec0a401e4a97b7bbc59ecd7b1c2c
Jules Bouvet <j.bouvet99@gmail.com>
2021-03-19 14:59:09 +0100

    Update

--- Generating Sound with NN/Test with BrainSong/main.py
+++
@@ -1,8 +0,0 @@
-from audio_autoencoder import VAE
-from torchsummary import summary
-from training import train_model
-
-model = VAE()
-summary(model, input_size=(2, 94080))
-#model_trained = train_model(model, batchsize=4)
-
